81d35daefd56bdb54c4b776f27c30050a4f07b6c
Sam Kortekaas <samkortekaas@protonmail.com>
2020-09-10 10:41:57 +0200

    Updated announcement function to update death state

--- philo_one/utils.c
+++ philo_one/utils.c
@@ -56,7 +56,7 @@ int announce(char *message)
 	return (0);
 }
 
-int safe_announce(char *message, t_philo *philo)
+int safe_announce(char *message, t_philo *philo, int death)
 {
 	t_data	*data;
 
@@ -64,12 +64,17 @@ int safe_announce(char *message, t_philo *philo)
 	pthread_mutex_lock(&data->write_mutex);
 	if (data->state == ALIVE && philo->amount_eaten != data->n_meals)
 	{
-		ft_putnbr_fd(get_time() - data->start_time, 1);
+		ft_putnbr_fd(get_time() - philo->data->start_time, 1);
 		write(1, "\t", 1);
 		ft_putnbr_fd(philo->id, 1);
 		write(1, " ", 1);
 		write(1, message, ft_strlen(message));
 		write(1, "\n", 1);
+	}
+	if (death)
+	{
+		philo->data->state = DEATH;
+		usleep(500);
 	}
 	pthread_mutex_unlock(&data->write_mutex);
 	return (0);
@@ -83,11 +88,18 @@ uint64_t	get_time()
 	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
-void	my_usleep(uint64_t wait)
+void my_usleep(uint64_t wait, t_philo *philo)
 {
 	uint64_t	start;
 
 	start = get_time();
 	while ((get_time() - start) < wait)
+	{
+		if ((get_time() - philo->last_eaten) > philo->data->t_die)
+		{
+			safe_announce("died.", philo, 1);
+			break;
+		}
 		usleep(100);
+	}
 }
